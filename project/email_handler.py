d86c27b9cb3eeafe79bf864be0a2946c14a32fba
segovelo <gauchosinpingo@gmail.com>
2022-10-22 19:42:54 +0100

    sendFile() with attachment

--- project/email_handler.py
+++ project/email_handler.py
@@ -2,7 +2,7 @@
 from email.message import EmailMessage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
+from email.mime.application import MIMEApplication
 from email import encoders
 #from re import sub
 import ssl
@@ -15,6 +15,7 @@ load_dotenv()
 email_sender = os.getenv("EMAIL_SENDER")
 email_password = os.getenv("EMAIL_PASSWORD")
 email_receiver = os.getenv("EMAIL_RECEIVER")
+email_cc = os.getenv("EMAIL_CC")
 
 
 class Email():
@@ -32,15 +33,51 @@ class Email():
             em = EmailMessage()
             em['From'] = email_sender
             em['To'] = email_receiver
+            em['CC'] = email_cc
             em['subject'] = subject
             em.set_content(body)
 
             context = ssl.create_default_context()
             with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smpt:
                 smpt.login(email_sender, email_password)
-                smpt.sendmail(email_sender, email_receiver, em.as_string())
+                smpt.sendmail(email_sender, email_receiver.split(
+                    ";") + (email_cc.split(";") if email_cc else []), em.as_string())
+                smpt.quit()
             print("Email was sent Successfully...")
         except Exception as ex:
             print(f'Exception Occured ! \n {ex}')
 
-    # def sendFile(self, file_path=None):
+    def sendFile(self, file_path=None):
+        print('Sending email...')
+        try:
+            smtp_server = 'smtp.gmail.com'
+            smtp_port = 587
+            subject = 'Email Attachment Automation with Python'
+            em = MIMEMultipart('mixed')
+            em['From'] = 'Contact <{sender}>'.format(sender=email_sender)
+            em['To'] = email_receiver
+            em['CC'] = 'sebastian_gvl@hotmail.com'
+            em['subject'] = subject
+
+            em_content = '<h4>Hello {email_sender},<br> This is a test email with attachment, send from python script to automate the process.</h4>\n'
+            body = MIMEText(em_content, 'html')
+            em.attach(body)
+
+            with open(file_path, "rb") as attachment:
+                p = MIMEApplication(attachment.read(), _subtype="pdf")
+                p.add_header(
+                    'Content-Disposition', "attachment; filename= %s" % file_path.split("\\")[-1])
+                em.attach(p)
+                attachment.close()
+
+            context = ssl.create_default_context()
+            with smtplib.SMTP_SSL(smtp_server, smtp_port, context=context) as smpt:
+                smpt.ehlo()
+                smpt.login(email_sender, email_password)
+                smpt.sendmail(email_sender, email_receiver.split(
+                    ";") + (email_cc.split(";") if email_cc else []), em.as_string())
+                smpt.quit()
+
+            print("Email was sent Successfully...")
+        except Exception as ex:
+            print(f'Exception Occured ! \n {ex}')
